d841c3ec6cf2f80c9fc7876ea647116e986dd303
Nil Borodulia <nil.b@oxtech.org>
2022-07-22 10:27:52 +0100

    full template

--- output.php
+++ output.php
@@ -19,4 +19,22 @@ class UserEntity
         return $this->name;
     }
 
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+
+    public static function fromArray(array $fields): self
+    {
+        $entity = new self();
+
+        $entity->id = $fields['id'];
+        $entity->name = $fields['name'];
+
+        return $entity;
+    }
 }
