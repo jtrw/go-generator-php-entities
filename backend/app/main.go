45fbe0e65d738004c516e4d019086254e2d8db68
Nil Borodulia <nil.b@oxtech.org>
2022-07-22 08:02:20 +0100

    add template

--- backend/app/main.go
+++ backend/app/main.go
@@ -3,12 +3,13 @@ package main
 import (
   //"fmt"
   "log"
-  //"os"
+  "os"
   "github.com/jessevdk/go-flags"
   //"net/http"
   //"io/ioutil"
   //"bytes"
   //"github.com/joho/godotenv"
+  "text/template"
 )
 
 type Options struct {
@@ -24,5 +25,77 @@ func main() {
 
     if err != nil {
         log.Fatal(err)
+    }
+
+    const tmp = `
+        private {{.Type}} {{.Name}};`
+
+    type TemplateEntity struct {
+        Type, Name string
+    }
+
+    var templateData = []TemplateEntity{
+        {"int", "$id"},
+        {"string", "$name"},
+    }
+
+    t := template.Must(template.New("template").Parse(tmp))
+
+    // Execute the template for each recipient.
+
+    for _, r := range templateData {
+        err := t.Execute(os.Stdout, r)
+        if err != nil {
+            log.Println("executing template:", err)
+        }
+    }
+
+    printEntity();
+}
+
+func printEntity() {
+//     const tmp = `<?php
+// namespace App\Entity;
+//
+// class {{.EntityName}}
+// {
+//     public function {{.MethodName}}()
+//     {
+//         return $this->id;
+//     }
+// }
+// `
+
+    type TemplateEntity struct {
+        EntityName, MethodName string
+    }
+
+    var templateData = []TemplateEntity{
+        {"UserEntity", "getId"},
+    }
+
+    t, err := template.ParseFiles("backend/app/template/entity.gohtml")
+    if err != nil {
+       panic(err)
+    }
+    //t := template.Must(template.New("template").Parse(tmp))
+
+    fo, err := os.Create("output.php")
+    if err != nil {
+        panic(err)
+    }
+    // close fo on exit and check for its returned error
+    defer func() {
+        if err := fo.Close(); err != nil {
+            panic(err)
+        }
+    }()
+
+    // Execute the template for each recipient.
+    for _, r := range templateData {
+        err := t.Execute(fo, r)
+        if err != nil {
+            log.Println("executing template:", err)
+        }
     }
 }
---
+++ output.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Entity;
+
+class UserEntity
+{
+    public function getId()
+    {
+        return $this->id;
+    }
+}
