b36554dc7a959bd17c2f22eb757483d57565aa5a
Nil Borodulia <nil.b@oxtech.org>
2022-07-25 12:58:49 +0100

    add generator entiry

---
+++ backend/app/generator/field.go
@@ -0,0 +1,6 @@
+package generator
+
+type Info struct {
+	Field string
+	Type string
+}
--- backend/app/main.go
+++ backend/app/main.go
@@ -3,16 +3,16 @@ package main
 import (
   //"fmt"
   "log"
-  "os"
+  //"os"
   "github.com/jessevdk/go-flags"
   //"net/http"
   //"io/ioutil"
   //"bytes"
   //"github.com/joho/godotenv"
-  "text/template"
   // "database/sql"
-   describe "generator-php-entities/v1/backend/app/repository"
+   describe "generator-php-entities/v1/backend/app/db/repository"
    connection "generator-php-entities/v1/backend/app/db"
+   entity "generator-php-entities/v1/backend/app/generator"
    "strings"
 )
 
@@ -23,9 +23,13 @@ type Options struct {
    DbUser string `short:"u" long:"db_user" default:"mysql_user" description:"DB User"`
    DbPassword string `long:"db_password" default:"astalavista" description:"DB Password"`
    DbType string `long:"db_type" default:"mysql" description:"Type of DB"`
+
+   Type string `short:"y" long:"type" default:"entity" description:"Type of generates files"`
    Table string `short:"t" long:"table" required:"true" description:"Table for generate Entity"`
    StoragePath string `short:"s" long:"storage_path" default:"/var/tmp/jtrw_generator_php_entities.db" description:"Storage Path"`
 }
+
+const TYPE_ENTITY string = "entity"
 
 func main() {
     var opts Options
@@ -55,100 +59,14 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
+    if opts.Type == TYPE_ENTITY {
+        entity.Generate(getPreparedEntityName(opts.Table), results)
+    } else {
+        log.Fatal("Type of generate files not found")
+    }
 
-    generateEntity(getPreparedEntityName(opts.Table), results);
 }
 
-func generateEntity(entityName string, rows []describe.DescribeTable) {
-    type Property struct {
-        Type, Name string
-    }
-
-    type Method struct {
-        MethodName, TypeMethod, Return string
-    }
-
-    type TemplateEntity struct {
-        Properties []Property
-        Methods []Method
-        EntityName string
-    }
-
-     var PropertiesData = []Property{}
-     var MethodsData = []Method{}
-
-     for _, row := range rows {
-         var rowData Property
-         var rowMethod Method
-        propertyName := getPreparedName(row.Field)
-        propertyType := getPreparedType(row.Type)
-        rowData.Type = propertyType
-        rowData.Name = "$"+propertyName
-        PropertiesData = append(PropertiesData, rowData)
-
-        rowMethod.MethodName = "get"+strings.Title(propertyName)
-        rowMethod.TypeMethod = propertyType
-        rowMethod.Return = propertyName;
-
-        MethodsData = append(MethodsData, rowMethod)
-
-    }
-
-    var templateData = TemplateEntity{
-        EntityName: entityName,
-        Properties: PropertiesData,
-        Methods: MethodsData,
-    }
-
-    t, err := template.ParseFiles("backend/app/template/entity.gohtml")
-    if err != nil {
-       panic(err)
-    }
-
-    fo, err := os.Create(entityName+".php")
-    if err != nil {
-        panic(err)
-    }
-    // close fo on exit and check for its returned error
-    defer func() {
-        if err := fo.Close(); err != nil {
-            panic(err)
-        }
-    }()
-
-    err = t.Execute(fo, templateData)
-    if err != nil {
-        log.Println("executing template:", err)
-    }
-}
-
-func getPreparedType(t string) (string) {
-
-    if strings.Contains(t, "int") {
-        return "int"
-    }
-
-    if strings.Contains(t, "decimal") || strings.Contains(t, "float") {
-        return "float"
-    }
-
-    if strings.Contains(t, "varchar") || strings.Contains(t, "enum") ||
-     strings.Contains(t, "time") || strings.Contains(t, "char") ||
-     strings.Contains(t, "date") || strings.Contains(t, "text") {
-        return "string"
-    }
-
-    return t
-}
-
-func getPreparedName(name string) (string) {
-    words := strings.Split(name, "_")
-    name = strings.ToLower(words[0])
-    for _, word := range words[1:] {
-        name += strings.Title(word)
-    }
-    return name;
-}
 
 func getPreparedEntityName(name string) (string) {
     return strings.Title(name)+"Entity"
